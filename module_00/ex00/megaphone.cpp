57e9a34ff68cb0e5dd495d942a4d2fb50a3a6781
Valeria Vagapova <lera.vagapova@gmail.com>
2023-10-17 19:59:00 +0300

    add ex00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Megaphone
+{
+
+public:
+
+	Megaphone(void)
+	{
+		return;
+	}
+
+	~Megaphone(void)
+	{
+		return;
+	}
+
+	void activate(int ac, char** av)
+	{
+		if (ac <= 1)
+		{
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		}
+		else
+		{
+			for (int i=1; i<ac; i++)
+				printUpper(av[i]);
+			std::cout << std::endl;
+		}
+	}
+
+private:
+
+	void printUpper(char* arg)
+	{
+		std::string str = arg;
+		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+		std::cout << str;
+	}
+
+};
+
+int main(int ac, char** av)
+{
+	Megaphone megaphone;
+
+	megaphone.activate(ac, av);
+}
