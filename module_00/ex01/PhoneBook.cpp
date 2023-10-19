1c92f11819906e3c53781af9f618f63c7c526b4c
Valeria Vagapova <lera.vagapova@gmail.com>
2023-10-19 19:38:05 +0300

    add nice retro ascii art

--- module_00/ex01/PhoneBook.cpp
+++ module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 16:32:00 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/10/19 17:48:34 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/10/19 19:37:34 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,20 @@ PhoneBook::~PhoneBook(void)
 
 void PhoneBook::printInstructions(void)
 {
-	std::cout << "[instructions will be here]" << std::endl;
+	std::cout << "   ___________________________________________" << std::endl;
+	std::cout << " / \\                                          \\" << std::endl;
+	std::cout << "|   |  WELCOME TO YOUR CRAPPY 80S PHONEBOOK!  |" << std::endl;
+	std::cout << " \\_ |                                         |" << std::endl;
+	std::cout << "    |  Here's the little that it can do:      |" << std::endl;
+	std::cout << "    |                                         |" << std::endl;
+	std::cout << "    |    ‣ ADD - Adds a new contact           |" << std::endl;
+	std::cout << "    |    ‣ SEARCH - Displays saved contacts   |" << std::endl;
+	std::cout << "    |    ‣ EXIT - The program quits and       |" << std::endl;
+	std::cout << "    |        your contacts are lost FOREVER!  |" << std::endl;
+	std::cout << "    |   ______________________________________|___" << std::endl;
+	std::cout << "    |  /                                         /" << std::endl;
+	std::cout << "    \\_/_________________________________________/" << std::endl;
+	std::cout << std::endl;
 }
 
 void PhoneBook::add(void)
