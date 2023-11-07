ec9a87dab8473be39f02b1d51b6e8135f516729a
Valeria Vagapova <lera.vagapova@gmail.com>
2023-11-07 17:58:32 +0200

    fix makefile and add animal class header

--- module_04/ex00/Animal.hpp
+++ module_04/ex00/Animal.hpp
@@ -6,7 +6,27 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:26:57 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/11/07 17:26:58 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/11/07 17:54:16 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <string>
+
+class Animal
+{
+protected:
+	std::string type;
+	virtual void makeSound(void);
+
+public:
+	Animal(void);
+	Animal(const Animal &source);
+	~Animal(void);
+
+	Animal &operator=(const Animal &source);
+};
+
+#endif
