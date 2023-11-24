5c18517103111bf4290919648a077291164de4e1
Valeria Vagapova <lera.vagapova@gmail.com>
2023-11-24 18:02:04 +0200

    add brain methods for testing

--- module_04/ex01/Brain.cpp
+++ module_04/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:21:34 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/11/14 17:20:16 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/11/24 18:01:16 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Brain::Brain(void) {}
 
 Brain::Brain(const Brain &source)
 {
-	for (int i=0; i<100; i++)
+	for (int i = 0; i < 100; i++)
 		this->ideas[i] = source.ideas[i];
 }
 
@@ -24,7 +24,23 @@ Brain::~Brain(void) {}
 
 Brain &Brain::operator=(const Brain &source)
 {
-	for (int i=0; i<100; i++)
+	for (int i = 0; i < 100; i++)
 		this->ideas[i] = source.ideas[i];
 	return *this;
+}
+
+void Brain::addIdea(std::string newIdea, int index)
+{
+	if (index < 0 || index >= 100)
+		return;
+	this->ideas[index] = newIdea;
+}
+
+void Brain::printIdeas(void) const
+{
+	for (int i = 0; i < 100; i++)
+	{
+		if (this->ideas[i].length())
+			std::cout << "[" << i << "] " << this->ideas[i] << std::endl;
+	}
 }
--- module_04/ex01/Brain.hpp
+++ module_04/ex01/Brain.hpp
@@ -6,14 +6,14 @@
 /*   By: vvagapov <vvagapov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:21:39 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/11/14 16:23:42 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/11/24 18:01:10 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 #define BRAIN_HPP
 
-#include <string>
+#include <iostream>
 
 class Brain
 {
@@ -25,6 +25,10 @@ public:
 	Brain(const Brain &source);
 	~Brain(void);
 	Brain &operator=(const Brain &source);
+
+	// functions for testing
+	void addIdea(std::string newIdea, int index);
+	void printIdeas(void) const;
 };
 
 #endif
