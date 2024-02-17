d5140d6f58da535ce960822b6ad2d6a2ed488511
Valeria Vagapova <lera.vagapova@gmail.com>
2024-02-17 20:32:27 +0200

    add cleaning up shrubbery to makefiles, improve intern tests

--- module_05/ex03/main.cpp
+++ module_05/ex03/main.cpp
@@ -13,13 +13,22 @@ int main(void)
 		AForm* shrubbery;
 		AForm* robotomy;
 		AForm* presidential;
-		AForm* wrong;
+
 		shrubbery = someRandomIntern.makeForm("shrubbery", "ShrubberyTarget");
 		std::cout << *shrubbery << std::endl;
 		robotomy = someRandomIntern.makeForm("robotomy", "RobotomyTarget");
 		std::cout << *robotomy << std::endl;
 		presidential = someRandomIntern.makeForm("presidential", "PresidentialTarget");
 		std::cout << *presidential << std::endl;
+
+		// FORM EXECUTION TEST
+
+		Bureaucrat	CEOBureaucrat("CEOBureaucrat", 1);
+		CEOBureaucrat.executeForm(*shrubbery);
+		CEOBureaucrat.executeForm(*robotomy);
+		CEOBureaucrat.executeForm(*presidential);
+
+		AForm* wrong;
 		wrong = someRandomIntern.makeForm("wrong", "WrongTarget");
 		std::cout << *wrong << std::endl;
 	}
