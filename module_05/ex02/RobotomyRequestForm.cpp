4aefe6d8856494ec63e0524bc079694fa1a93906
Valeria Vagapova <lera.vagapova@gmail.com>
2024-02-17 16:23:02 +0200

    add robotomy form

---
+++ module_05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,42 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm()
+	: RobotomyRequestForm("DefaultTarget") {}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string newTarget)
+	: AForm("RobotomyRequestForm", 25, 5, newTarget) {}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &source) : AForm(source) {}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(RobotomyRequestForm const &source)
+{
+	AForm::operator=(source);
+	return (*this);
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const &executor) const
+{
+	// TODO: refactor to move grade check to the base class	
+	if (executor.getGrade() > this->getGradeToExecute())
+	{
+		throw GradeTooLowException();
+	}
+	std::string drilling = "* drilling noises *";
+	for (unsigned int i = 0; i < drilling.length(); i++)
+	{
+		std::cout << drilling[i] << std::flush;
+		usleep(50000);
+	}
+	std::cout << std::endl << std::flush; 
+	usleep(500000);
+	if (rand() % 2)
+	{
+		std::cout << this->getTarget() << " has been robotomized successfully" << std::endl;
+	}
+	else
+	{
+		std::cout << "Robotomy failed :(" << std::endl;
+	}
+}
--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -2,6 +2,7 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main(void)
 {
@@ -14,14 +15,17 @@ int main(void)
 		Bureaucrat	CEOBureaucrat("CEOBureaucrat", 1);
 		std::cout << CEOBureaucrat << std::endl;
 
-		ShrubberyCreationForm form("shrubberyTarget");
+		ShrubberyCreationForm form("ShrubberyTarget");
 		std::cout << form << std::endl;
 		bureaucrat137.signForm(form);
 		std::cout << form << std::endl;
 		bureaucrat137.executeForm(form);
-		//internBureaucrat.executeForm(form);
 
-		PresidentialPardonForm presidentialForm("presidentialTarget");
+		RobotomyRequestForm robotomyFrom("RobotomyTarget");
+		CEOBureaucrat.signForm(robotomyFrom);
+		CEOBureaucrat.executeForm(robotomyFrom);
+
+		PresidentialPardonForm presidentialForm("PresidentialTarget");
 		CEOBureaucrat.signForm(presidentialForm);
 		std::cout << presidentialForm << std::endl;
 		CEOBureaucrat.executeForm(presidentialForm);
