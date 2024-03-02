b05b67241109c78a28657e346e31095b8f928d50
Valeria Vagapova <lera.vagapova@gmail.com>
2024-03-02 15:32:09 +0200

    add untested Span code

--- module_08/ex01/Span.cpp
+++ module_08/ex01/Span.cpp
@@ -1,10 +1,73 @@
 /* int min=a[0],ans=0;
 for (int i=1;i<n;i++)
-    if (a[i]<min) min=a[i];
-    else ans=max(ans,a[i]-min);
+	if (a[i]<min) min=a[i];
+	else ans=max(ans,a[i]-min);
 return ans; */
 
 #include "Span.hpp"
 
-template<typename T>
 Span::Span() : N(0) {}
+
+Span::Span(unsigned int newN) : N(newN) {}
+
+Span::Span(const Span & source) : N(source.N)
+{
+	this->span = source.span;
+}
+
+Span::~Span() {}
+
+Span & Span::operator=(const Span & source)
+{
+	this->span = source.span;
+	this->N = source.N;
+}
+
+void Span::addNumber(int newNum)
+{
+	if (this->span.size() + 1 >= this->N)
+		throw OverCapacityException();
+	this->span.insert(newNum);
+}
+
+template<typename Container>
+void addNumber(typename Container::const_iterator & range)
+{
+	if ((range.end() - range.begin()) + this->N > N)
+		throw OverCapacityException();
+	this->span.insert_range(range);
+}
+
+unsigned int Span::shortestSpan()
+{
+	if (N < 2)
+		throw NotEnoughElementsException();
+
+	unsigned int minDiff = *next(this->span.begin()) - *this->span.begin();
+
+	std::multiset<int, std::greater<int>>::iterator itr;
+	for (itr = this->span.begin(); itr != prev(this->span.end()); ++itr)
+	{
+		unsigned int newDiff = (*next(itr) - *itr);
+		if (newDiff < minDiff)
+			minDiff = newDiff;
+	}
+	return minDiff;
+}
+
+unsigned int Span::longestSpan()
+{
+	if (N < 2)
+		throw NotEnoughElementsException();
+	return (*this->span.end() - *this->span.begin());
+}
+
+const char * Span::OverCapacityException::what() const throw()
+{
+	return "ERROR: The Span is full, can't add more numbers.";
+}
+
+const char * Span::NotEnoughElementsException::what() const throw()
+{
+	return "ERROR: Not enough elements in the Span to calculate shortest/longest span.";
+}
--- module_08/ex01/Span.hpp
+++ module_08/ex01/Span.hpp
@@ -1,24 +1,43 @@
-template<typename T>
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <set>
+
 class Span
 {
 	private:
 		unsigned int N;
+		std::multiset<int, std::greater<int>> span;
 	public:
 		Span();
+		Span(unsigned int newN);
 		Span(const Span & source);
 		~Span();
 
 		Span &operator=(const Span & source);
 
 		void addNumber(int newNum);
-		void addNumber(const T::iterator & iterStart,
-			const T::iterator & iterEnd);
-		int shortestSpan();
-		int longestSpan();
+
+		/* template<typename Container>
+		void addNumber(typename Container::const_iterator & iterStart,
+			typename Container::const_iterator & iterEnd); */
+		template<typename Container>
+		void addNumber(typename Container::const_iterator & range);
+
+		unsigned int shortestSpan();
+		unsigned int longestSpan();
 
 		class OverCapacityException : public std::exception
 		{
 			public:
 				virtual const char * what() const throw();
 		};
+
+		class NotEnoughElementsException : public std::exception
+		{
+			public:
+				virtual const char * what() const throw();
+		};
 };
+
+#endif
